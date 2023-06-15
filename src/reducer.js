ce8e593a8104e9f075d651a30d05e995eebd8279
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-06-15 16:09:37 +0300

    initial commit

--- src/reducer.js
+++ src/reducer.js
@@ -6,31 +6,38 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+
   switch (action.type) {
     case 'GOOD':
     const state_2 = {
-      good: initialState.good +1,
-      ok: initialState.ok,
-      bad: initialState.bad
+      good: state.good + 1,
+      ok: state.ok,
+      bad: state.bad
     }
     return state_2
     case 'OK':
       const state_3 = {
-        good: initialState.good,
-        ok: initialState.ok + 1,
-        bad: initialState.bad
+        good: state.good,
+        ok: state.ok + 1,
+        bad: state.bad
       }
       return state_3
     case 'BAD':
       const state_4 = {
-        good: initialState.good,
-        ok: initialState.ok,
-        bad: initialState.bad + 1
+        good: state.good,
+        ok: state.ok,
+        bad: state.bad + 1
       }  
       return state_4
     case 'ZERO':
-      return state
+
+    const state_zero = {
+      good: 0,
+      ok: 0,
+      bad: 0
+    }  
+    console.log('zeroing shit', state_zero)
+      return state_zero
     default: return state
   }
   
